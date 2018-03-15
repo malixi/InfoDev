68013cd8c38e16d4d2bae5d45d4379589d0a9311
Carlo <malixi@users.noreply.github.com>
2018-03-15 17:33:05 +0800

    Remove unwanted files

--- update.php
+++ update.php
@@ -1,8 +1,10 @@
 <?php
-//update.php
+
 $connect = mysqli_connect("localhost", "root", "", "infodev");
+	
 $query = "
- UPDATE product SET ".$_POST["name"]." = '".$_POST["value"]."' 
- WHERE id = '".$_POST["pk"]."'";
-mysqli_query($connect, $query);
+	 UPDATE product SET ".$_POST["name"]." = '".$_POST["value"]."' 
+	 WHERE id = '".$_POST["pk"]."'";
+	mysqli_query($connect, $query);
+
 ?>
